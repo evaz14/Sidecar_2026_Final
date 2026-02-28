1a9d9249561d61953a350199efc98e989485194c
Eva Zheng <eva.zheng2142147@gmail.com>
2026-02-27 23:14:55 -0500

    shifts and timers (no gopro)

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import time
 import tkinter as tk
-import sys
 from tkinter import font as tkfont
 
 import wpilib
+import threading
 
 from wpilib import DriverStation
 
 from networktables import NetworkTables
 from open_gopro import WirelessGoPro
 
-# visual gui
+# global variables
+secondsLeftTeleop = 0
+secondsLeftAuton = 0
+data = ""
 
+# visual gui
 window = tk.Tk() # create window
 window.geometry("1300x800") # size
 window.title("Sidecar") # title
@@ -27,7 +31,6 @@ isEnabledStatusLabel = tk.Label(
     bg = "red",
     fg = "black"
 )
-
 isEnabledStatusLabel.place(x=350, y=25, height=200, width=600)
 
 # timer in game
@@ -38,7 +41,6 @@ GameTimeLeftLabel = tk.Label(
     bg = "light blue",
     fg = "black"
 )
-
 GameTimeLeftLabel.place(x=350, y=275, height=200, width=600)
 
 # autnomous/teleoperated
@@ -49,7 +51,6 @@ GamePeriodLabel = tk.Label(
     bg = "light blue",
     fg = "black"
 )
-
 GamePeriodLabel.place(x=25, y=525, height=200, width=600)
 
 # shift of the game, transition, scoring, etc.
@@ -60,59 +61,86 @@ GameShiftLabel = tk.Label(
     bg = "light blue",
     fg = "black"
 )
-
 GameShiftLabel.place(x=650, y=525, height=200, width=600)
 
 # networktables
-
 def connectionListener(connected, info):
     print(info, "; Connected=%s" % connected)
 
-# main method
+def timerCountdownTeleop():
+    global secondsLeftTeleop
+    secondsLeftTeleop = 140
+    while secondsLeftTeleop >= 0:
+        time.sleep(1)
+        secondsLeftTeleop -= 1
+
+def timerCountdownAuton():
+    global secondsLeftAuton
+    secondsLeftAuton = 20
+    while secondsLeftAuton >= 0:
+        time.sleep(1)
+        secondsLeftAuton -= 1
+
+# enabled/disabled, auton/teleop
 def valueChanged(table, key, value, isNew): 
-    global isEnabledStatus
-    
+    global data, secondsLeftTeleop, secondsLeftAuton
     if key == "FMSControlData":
         if value == 32:
-            isEnabledStatusLabel.config(text = "DISABLED", bg = "red", fg = "black")
+            isEnabledStatusLabel.config(text = "DISABLED", bg = "red")
             GamePeriodLabel.config(text = "TELEOP")
-        if value == 33:
-            isEnabledStatusLabel.config(text = "ENABLED", bg = "green", fg = "black")
+        elif value == 33:
+            isEnabledStatusLabel.config(text = "ENABLED", bg = "green")
             GamePeriodLabel.config(text = "TELEOP")
-        if value == 34:
-            isEnabledStatusLabel.config(text = "DISABLED", bg = "red", fg = "black")
+            threading.Thread(target=timerCountdownTeleop, daemon=True).start()
+        elif value == 34:
+            isEnabledStatusLabel.config(text = "DISABLED", bg = "red")
             GamePeriodLabel.config(text = "AUTON")
-        if value == 35:
-            isEnabledStatusLabel.config(text = "ENABLED", bg = "green", fg = "black")
+        elif value == 35:
+            isEnabledStatusLabel.config(text = "ENABLED", bg = "green")
             GamePeriodLabel.config(text = "AUTON")
+            threading.Thread(target=timerCountdownAuton, daemon=True).start()
     
     print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
 
+def periodic():
+    global data
+    data = wpilib.DriverStation.getGameSpecificMessage()
+    alliance = wpilib.DriverStation.getAlliance()
+    
+    GameShiftLabel.config(bg = "light blue") # may cause flicker, change if needed
 
+    if wpilib.DriverStation.isTeleop():
+        if secondsLeftTeleop > 130:
+            GameShiftLabel.config(text = "Transition Period", bg = "light blue")
+            GameTimeLeftLabel.config(text = str(secondsLeftTeleop - 130))
+        elif 105 < secondsLeftTeleop <= 130:
+            GameShiftLabel.config(text = "Shift 1")
+            GameTimeLeftLabel.config(text = str(secondsLeftTeleop - 105))
+            if data == "R" and alliance == wpilib.DriverStation.Alliance.kBlue or data == "B" and alliance == wpilib.DriverStation.Alliance.kRed:
+                        GameShiftLabel.config(bg = "green", fg = "black")
+        elif 80 < secondsLeftTeleop <= 105:
+            GameShiftLabel.config(text = "Shift 2")
+            GameTimeLeftLabel.config(text = str(secondsLeftTeleop - 80))
+            if data == "R" and alliance == wpilib.DriverStation.Alliance.kRed or data == "B" and alliance == wpilib.DriverStation.Alliance.kBlue:
+                GameShiftLabel.config(bg = "green", fg = "black")
+        elif 55 < secondsLeftTeleop <= 80:
+            GameShiftLabel.config(text = "Shift 3")
+            GameTimeLeftLabel.config(text = str(secondsLeftTeleop - 55))
+            if data == "R" and alliance == wpilib.DriverStation.Alliance.kBlue or data == "B" and alliance == wpilib.DriverStation.Alliance.kRed:
+                GameShiftLabel.config(bg = "green", fg = "black")
+        elif 30 < secondsLeftTeleop <= 55:
+            GameShiftLabel.config(text = "Shift 4")
+            GameTimeLeftLabel.config(text = str(secondsLeftTeleop - 30))
+            if data == "R" and alliance == wpilib.DriverStation.Alliance.kRed or data == "B" and alliance == wpilib.DriverStation.Alliance.kBlue:
+                GameShiftLabel.config(bg = "green", fg = "black")
+        elif 0 <= secondsLeftTeleop <= 30:
+            GameShiftLabel.config(text = "Endgame", bg = "light blue", fg = "black")
+            GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime()))
+    elif wpilib.DriverStation.isAuton():
+            GameShiftLabel.config(text = "Autonomous Period")
+            GameTimeLeftLabel.config(text = str(secondsLeftAuton))
 
-def periodic(intervalSeconds):
-    while True:
-        if wpilib.DriverStation.isTeleop():
-                if wpilib.DriverStation.getMatchTime() > 130:
-                    GameShiftLabel.config(text = "Transition Period")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime()))
-                if 130 <= wpilib.DriverStation.getMatchTime() > 105:
-                    GameShiftLabel.config(text = "Shift 1")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime() - 10))
-                if 105 <= wpilib.DriverStation.getMatchTime() > 80:
-                    GameShiftLabel.config(text = "Shift 2")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime() - 35))
-                if 80 <= wpilib.DriverStation.getMatchTime() > 55:
-                    GameShiftLabel.config(text = "Shift 3")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime() - 60))
-                if 55 <= wpilib.DriverStation.getMatchTime() > 30:
-                    GameShiftLabel.config(text = "Shift 4")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime() - 85))
-                if 30 >= wpilib.DriverStation.getMatchTime() >= 0:
-                    GameShiftLabel.config(text = "Endgame")
-                    GameTimeLeftLabel.config(text = str(wpilib.DriverStation.getMatchTime() - 110))
-
-        time.sleep(intervalSeconds)
+    window.after(200, periodic)
     
 
 # Set the RoboRIO IP address
@@ -131,12 +159,5 @@ if NetworkTables.isConnected():
 else:
     print("Failed to connect to NetworkTables server")
 
+periodic()
 window.mainloop()
-periodic(1)
-
-
-# enable teleop = 33
-# disabled auton = 34
-
-# disabled teleop = 32
-# enabled auton = 35
